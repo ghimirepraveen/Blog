3674213783ba64359718ff5da2a0ad1187b1e0b7
ghimirepraveen <ghimireprabin7777@gmail.com>
2024-05-26 19:52:18 +0545

    Prepare for deployment on Render

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    strictPort: true,
+    port: 8000,
+  },
+
   build: {
     outDir: "dist",
   },
